d424b8ef1b89a4c4ad3239ba46cab8f8044430f0
NoxusJr <ctt.georgejr@gmail.com>
2024-07-02 16:07:20 -0300

    Change logic for search global variables

--- Database/config/connection.php
+++ Database/config/connection.php
@@ -1,14 +1,14 @@
 <?php
 
 
-$dbHost = $_ENV['DB_HOST'];
-$dbUser = $_ENV['DB_USER'];
-$dbPass = $_ENV['DB_PASS'];
-$dbName = $_ENV['DB_NAME'];
+$dbHost = $_SERVER['DB_HOST'];
+$dbUser = $_SERVER['DB_USER'];
+$dbPass = $_SERVER['DB_PASS'];
+$dbName = $_SERVER['DB_NAME'];
 
-try{
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", "$dbUser", "$dbPass");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
-    echo "Erro ao tentar conectar ao banco de dados: ".$e->getMessage();
+try {
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro ao tentar conectar ao banco de dados: " . $e->getMessage();
 }
